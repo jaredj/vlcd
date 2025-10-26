f2609141e52fb7b559c95903c2575ffa265d6b04
Jared Johnson <jjohnson@axcient.com>
2025-10-26 13:31:27 -0500

    Increase Vitest thread pool

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
+import * as os from 'node:os';
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+
+const availableCpus = typeof os.availableParallelism === 'function' ? os.availableParallelism() : os.cpus().length;
+const maxThreads = Math.max(1, availableCpus * 4);
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
@@ -8,6 +12,13 @@ export default defineConfig(({ command }) => ({
     globals: true,
     environment: 'jsdom',
     setupFiles: './vitest.setup.ts',
+    pool: 'threads',
+    poolOptions: {
+      threads: {
+        minThreads: 1,
+        maxThreads,
+      },
+    },
     coverage: {
       provider: 'istanbul',
       reporter: ['text', 'lcov', 'json-summary'],
